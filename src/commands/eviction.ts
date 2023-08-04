88bec2ea3e8d002ab62da0e8627c56b90451c17d
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-08-04 13:57:21 -0500

    Refactor.

--- src/commands/eviction.ts
+++ src/commands/eviction.ts
@@ -5,15 +5,19 @@ import {
   readConfigurationFromDefaultPath,
 } from "../utils/configuration.js";
 import { wrapMessage } from "../utils/format.js";
-import { getTwilioClient } from "../utils/twilio.js";
+import { getTwilioClientFromConfiguration } from "../utils/twilio.js";
 
 export async function eviction(serverName: string): Promise<void> {
-  const config = readConfigurationFromDefaultPath();
-  const client = getTwilioClient();
+  console.log(chalk.blue.bold("Notifying administrators of eviction."));
 
+  const config = readConfigurationFromDefaultPath();
   const message = createMessage(config, serverName);
 
-  console.log(chalk.blue.bold("Notifying administrators of eviction."));
+  sendMessage(config, message);
+}
+
+function sendMessage(config: Configuration, message: string) {
+  const client = getTwilioClientFromConfiguration(config);
 
   config.toNumbers.forEach(async (number) => {
     await client.messages.create({
