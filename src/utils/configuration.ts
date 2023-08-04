f3b9587d653705fdbb791060f534de70677aef2e
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-08-03 19:47:24 -0500

    Fill out config template.

--- src/utils/configuration.ts
+++ src/utils/configuration.ts
@@ -1,3 +1,8 @@
 export const CONFIG_FILE_TEMPLATE = `{
-
+  "accountSid": "ACCOUNT_SID",
+  "authToken": "AUTH_TOKEN",
+  "messageServiceSid": "MESSAGE_SERVICE_SID",
+  "toNumbers": [
+    
+  ]
 }`;
