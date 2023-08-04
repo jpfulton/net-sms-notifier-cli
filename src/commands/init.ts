d622e5dfe3468e43ef24289c023c425eb758faad
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-08-03 19:52:32 -0500

    Move constants.

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -2,18 +2,19 @@ import chalk from "chalk";
 import fs from "fs";
 import path from "path";
 
-import { CONFIG_FILE_TEMPLATE } from "../utils/configuration.js";
-
-const configurationDir = "/etc/sms-notifier";
-const configurationFile = "notifier.json";
+import {
+  CONFIGURATION_DIR,
+  CONFIGURATION_FILE,
+  CONFIGURATION_FILE_TEMPLATE,
+} from "../utils/configuration.js";
 
 export async function init(): Promise<void> {
   console.log(chalk.blue.bold("Creating configuration directory and files."));
 
-  fs.access(configurationDir, (error) => {
+  fs.access(CONFIGURATION_DIR, (error) => {
     if (error) {
       console.log(chalk.blue("Creating configuration directory."));
-      fs.mkdir(configurationDir, 755, (error) => {
+      fs.mkdir(CONFIGURATION_DIR, 755, (error) => {
         if (error) {
           console.error(chalk.red("Failed to create configuration directory."));
           throw error;
@@ -25,31 +26,35 @@ export async function init(): Promise<void> {
   });
 
   const fullConfigurationFilePath = path.join(
-    configurationDir,
-    configurationFile
+    CONFIGURATION_DIR,
+    CONFIGURATION_FILE
   );
 
   fs.access(fullConfigurationFilePath, (error) => {
     if (error) {
       console.log(chalk.blue("Creating configuration file from template."));
-      fs.writeFile(fullConfigurationFilePath, CONFIG_FILE_TEMPLATE, (error) => {
-        if (error) {
-          console.error(
-            chalk.red("Failed to write configuration file template.")
-          );
-          throw error;
-        }
-
-        console.log(chalk.blue("Setting configuration file permissions."));
-        fs.chmod(fullConfigurationFilePath, 0o640, (error) => {
+      fs.writeFile(
+        fullConfigurationFilePath,
+        CONFIGURATION_FILE_TEMPLATE,
+        (error) => {
           if (error) {
             console.error(
-              chalk.red("Failed to set configuration file permissions.")
+              chalk.red("Failed to write configuration file template.")
             );
             throw error;
           }
-        });
-      });
+
+          console.log(chalk.blue("Setting configuration file permissions."));
+          fs.chmod(fullConfigurationFilePath, 0o640, (error) => {
+            if (error) {
+              console.error(
+                chalk.red("Failed to set configuration file permissions.")
+              );
+              throw error;
+            }
+          });
+        }
+      );
     } else {
       console.log(chalk.red("Configuration file exists."));
     }
--- src/utils/configuration.ts
+++ src/utils/configuration.ts
@@ -1,8 +1,11 @@
-export const CONFIG_FILE_TEMPLATE = `{
+export const CONFIGURATION_DIR = "/etc/sms-notifier";
+export const CONFIGURATION_FILE = "notifier.json";
+
+export const CONFIGURATION_FILE_TEMPLATE = `{
   "accountSid": "ACCOUNT_SID",
   "authToken": "AUTH_TOKEN",
   "messageServiceSid": "MESSAGE_SERVICE_SID",
   "toNumbers": [
-    
+
   ]
 }`;
