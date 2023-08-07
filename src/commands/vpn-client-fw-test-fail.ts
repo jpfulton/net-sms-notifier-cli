4adf7cf9bdcae7dfdfe581e930aed3bdf7bf651c
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-08-06 21:19:05 -0500

    Add new fw test fail command.

---
+++ src/commands/vpn-client-fw-test-fail.ts
@@ -0,0 +1,46 @@
+import chalk from "chalk";
+
+import {
+  Configuration,
+  readConfigurationFromDefaultPath,
+  validateConfigurationFromObject,
+} from "../utils/configuration.js";
+import { wrapMessage } from "../utils/format.js";
+import { sendMessage } from "../utils/twilio.js";
+
+export function vpnClientFirewallTestFail(options: {
+  ip: string;
+  certificateCN: string;
+}): void {
+  const { ip, certificateCN } = options;
+
+  console.log(
+    chalk.blue.bold(
+      "Notifying administrators of VPN client firewall test fail."
+    )
+  );
+
+  const config = readConfigurationFromDefaultPath();
+  if (!validateConfigurationFromObject(config)) {
+    console.error(chalk.red.bold("Invalid configuration file. Exiting."));
+    throw new Error("Invalid configuration file.");
+  }
+
+  const message = createMessage(config, ip, certificateCN);
+  sendMessage(config, message);
+}
+
+function createMessage(
+  config: Configuration,
+  ip: string,
+  certificateCN: string
+): string {
+  const now = new Date();
+  const time = now.toLocaleTimeString("en-US", {
+    timeStyle: "long",
+  });
+
+  const message = `VPN client firewall test fail from ${ip} using ${certificateCN} at ${time}.`;
+
+  return wrapMessage(config, message);
+}
